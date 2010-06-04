7e7245450212ebe29d858f19aa7c37fabe877109
Gary van Woerkens <gary@revolunet.com>
2010-06-04 21:07:52 +0200

    IE fix

---
+++ js/Ext.ux.Html5Connector.js
@@ -0,0 +1,35 @@
+/*
+** Ext.ux.Html5Connector.js for Ext.ux.Html5Connector in /home/gary/www/dev/upload
+**
+** Made by Gary van Woerkens
+** Contact   <gary@chewam.com>
+**
+** Started on  Fri Jun  4 19:02:46 2010 Gary van Woerkens
+** Last update Fri Jun  4 19:14:32 2010 Gary van Woerkens
+*/
+
+Ext.ns('Ext.ux');
+
+/**
+ * @class Ext.ux.Html5Connector
+ * @extends Ext.util.Observable
+ * The AuthPanel is a simple panel used as a container for two components :<br/>
+ * The application combobox to select available applications. <br/>
+ * The AuthTab panels displaying application's meta-groups.<br/>
+ * @author Gary van Woerkens
+ * @version 1.0
+ */
+
+Ext.ux.Html5Connector = function(config) {
+
+};
+
+Ext.extend(Ext.ux.Html5Connector, Ext.util.Observable, {
+
+  initComponent:function() {
+
+    Ext.ux.Html5Connector.superclass.initComponent.call(this);
+
+  }
+
+});
--- js/Ext.ux.Uploader.js
+++ js/Ext.ux.Uploader.js
@@ -5,7 +5,7 @@
 ** Contact <gary@chewam.com>
 **
 ** Started on  Wed May 26 17:45:41 2010 Gary van Woerkens
-** Last update Fri Jun  4 18:52:12 2010 Gary van Woerkens
+** Last update Fri Jun  4 20:43:44 2010 Gary van Woerkens
 */
 
 Ext.ns('Ext.ux');
@@ -31,8 +31,8 @@ Ext.ux.Uploader = function(config) {
     ,allowedFileTypes:this.allowedFileTypes
     ,maxFileSize:this.maxFileSize
     ,maxFiles:this.maxFiles
-    ,buttonImageUrl:"http://localhost/dev/upload/examples/img/button.png"
-    ,itemImageUrl:"/var/www/dev/upload/examples/img/menuitem.png"
+//    ,buttonImageUrl:"http://localhost/dev/upload/examples/img/button.png"
+//    ,itemImageUrl:"/var/www/dev/upload/examples/img/menuitem.png"
   });
 
   this.html5Params = config.html5Params || {};
@@ -212,12 +212,14 @@ Ext.extend(Ext.ux.Uploader, Ext.util.Observable, {
       el.setXY(this.el.getXY());
       el.setSize(width, height);
       if (conn.isLoaded)  {
+	/*
 	if (Ext.isChrome && conn.settings.buttonImageUrl) {
 	  var img, xtype = this.getXType();
 	  if (xtype === "button") img = conn.settings.buttonImageUrl;
 	  else if (xtype === "menuitem") img = conn.settings.itemImageUrl;
 	  //conn.setButtonImageURL(img);
 	}
+	 */
 	conn.setButtonDimensions(width, height);
       }
     }
@@ -229,7 +231,13 @@ Ext.extend(Ext.ux.Uploader, Ext.util.Observable, {
    * @param {Ext.Component} cmp The component to bind the trigger to.
    */
   ,setTrigger:function(cmp) {
-    var el = cmp.getEl().insertHtml("beforeEnd", '<div><div '
+    var cmpEl = cmp.getEl();
+      //    alert(cmpEl);
+    console.log(cmpEl.child("td.x-btn-mc"));
+    var btn = cmpEl.child("td.x-btn-mc");
+//    var tr = Ext.fly(cmpEl.first().dom.children[1]);
+//    console.log(cmpEl.first().dom.children[1].dom.children[1]);
+    var el = btn.insertHtml("beforeEnd", '<div><div '
       + 'style="'
       + 'position:absolute;'
       + 'cursor:pointer;'
@@ -238,7 +246,9 @@ Ext.extend(Ext.ux.Uploader, Ext.util.Observable, {
       + '></div>'
       + '</div></div>'
     , true);
+    console.log(el);
     cmp.uploadConfig = {};
+//    alert(cmp.getEl());
     cmp.uploadConfig.el = Ext.isChrome ? el : el.first();
     cmp.uploadConfig.body = Ext.isChrome ? el.first() : cmp.uploadConfig.el.first();
     cmp.uploadConfig.conn = this.getSwfConnector(cmp);
@@ -320,7 +330,7 @@ Ext.extend(Ext.ux.Uploader, Ext.util.Observable, {
    */
   ,getLogPanel:function() {
     if (!this.logPanel)
-      this.logPanel = new Ext.ux.uploadLogPanel({
+      this.logPanel = new Ext.ux.UploadLogPanel({
 	boundEl:this.boundEl
       });
     return this.logPanel;
@@ -425,232 +435,6 @@ Ext.extend(Ext.ux.Uploader, Ext.util.Observable, {
     });
     var reg = "^.*\.("+t.join("|")+")$";
     return new RegExp(reg, "g");
-  }
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * @class uploadLogPanel
- * @extends Ext.util.Observable
- * The AuthPanel is a simple panel used as a container for two components :<br/>
- * The application combobox to select available applications. <br/>
- * The AuthTab panels displaying application's meta-groups.<br/>
- * @author Gary van Woerkens
- * @version 1.0
- */
-
-Ext.ux.uploadLogPanel = function(config) {
-
-  Ext.apply(this, config);
-
-  this.panel = new Ext.Panel({
-    border:false
-    ,padding:"2"
-    ,defaults:{style:"margin:0 0 2 0"}
-    ,bbar:new Ext.ux.StatusBar({
-      height:22
-      ,style:"border:1px solid #99BBE8;"
-    })
-    ,autoScroll:true
-  });
-
-  Ext.ux.uploadLogPanel.superclass.constructor.apply(this, arguments);
-
-};
-
-Ext.extend(Ext.ux.uploadLogPanel, Ext.util.Observable, {
-
-  boundEl:null
-  ,win:null
-  ,queue:[]
-
-  ,progressTpl:new Ext.Template('<div class="x-progress-text-{type}">{text}</div>')
-
-  ,getWindow:function() {
-    if (this.boundEl) this.boundEl.mask();
-    if (!this.win) {
-      if (this.boundEl) {
-	this.win = new Ext.Panel({
-	  height:140
-	  ,width:350
-	  ,frame:true
-	  ,layout:"fit"
-	  ,items:[this.panel]
-	  ,floating:true
-	  ,tools:[{
-	    id:"close"
-	    ,scope:this
-	    ,handler:function(event, el, win){
-	      win.hide();
-	      this.boundEl.unmask();
-	    }
-	  }]
-	  ,listeners:{
-	    scope:this
-	    ,show:function(panel) {
-	      panel.el.anchorTo(this.boundEl, "c-c");
-	      panel.doLayout();
-	    }
-	  }
-	}).render(Ext.getBody());
-      } else {
-	this.win = new Ext.Window({
-	  height:200
-	  ,width:350
-	  ,layout:"fit"
-	  ,border:true
-	  ,closeAction:"hide"
-	  ,items:[this.panel]
-	});
-      }
-    }
-    return this.win;
-  }
-
-  ,getStatusBar:function() {
-    return this.panel.getBottomToolbar();
-  }
-
-  ,show:function(config) {
-    var scope = config.scope || null,
-    callback = config.callback || Ext.emptyFn,
-    toolbar = this.getStatusBar();
-    callback = scope ? callback.createDelegate(scope) : callback;
-    if (!this.win || this.win.hidden) {
-      toolbar.on({afterrender:callback});
-      this.getWindow().show();
-    } else {
-      callback();
-    }
-    var msg = config.fileCount + " " + (config.fileCount>1 ? "files" : "file") + " to upload";
-    this.log("info", msg);
-  }
-
-  ,add:function(file) {
-    var p = new Ext.ProgressBar({
-      text:this.progressTpl.apply({type:"loading", text:file.name})
-      ,isUploading:true
-    });
-    this.panel.add(p);
-    this.panel.doLayout();
-    this.queue.push({
-      id:file.id
-      ,p:p
-    });
-  }
-
-  ,log:function(type, msg) {
-    this.getStatusBar().setStatus({
-      text:msg
-      ,iconCls:"x-status-"+type
-    });
-  }
-
-  ,getProgress:function(id) {
-    var index = Ext.each(this.queue, function(item, index) {
-      return !(item.id === id);
-    });
-    return Ext.isDefined(index) ? this.queue[index].p : false;
-  }
-
-  ,updateProgress:function(file, progress, type) {
-    var toolbar = this.getStatusBar(),
-    p = this.getProgress(file.id);
-    if (progress === 1 || type == "error") {
-      type = type || "success";
-      p.isUploading = false;
-    }
-    type = type || "loading";
-    p.updateProgress(progress, this.progressTpl.apply({type:type, text:file.name}));
-    var count = this.getUploadingCount();
-    if (count) {
-      count = this.queue.length - count;
-      var msg = "envoie " + (this.queue.length > 1 ? "des fichiers" : "du fichier");
-      this.log("loading", msg+" ("+ count + "/" + this.queue.length+")");
-    } else {
-      count = this.queue.length - count;
-      var msg = "envoie terminé ";
-      this.log("info", msg+" ("+count + "/" + this.queue.length+")");
-    }
-  }
-
-  ,getUploadingCount:function() {
-    var count = 0;
-    Ext.each(this.queue, function(item) {
-      if (item.p.isUploading) count++;
-    });
-    return count;
-  }
-
-});
-
-Ext.reg('uploadlogspanel', Ext.ux.uploadLogPanel);
-
-
-
-
-
-
-Ext.ns('Ext.ux');
-/**
- * @class SwfConnector
- * @extends Ext.util.Observable
- * The AuthPanel is a simple panel used as a container for two components :<br/>
- * The application combobox to select available applications. <br/>
- * The AuthTab panels displaying application's meta-groups.<br/>
- * @author Gary van Woerkens
- * @version 1.0
- */
-Ext.ux.SwfConnector = function(config) {
-    //var config = getSwfConfig.call(cmp);
-    //cmp.swfUploader = new SWFUpload(config);
-};
-
-Ext.extend(Ext.ux.SwfConnector, Ext.util.Observable, {
-
-  initComponent:function() {
-
-    Ext.ux.SwfConnector.superclass.initComponent.call(this);
-
-  }
-
-});
-
-
-Ext.ns('Ext.ux');
-/**
- * @class Html5Connector
- * @extends Ext.util.Observable
- * The AuthPanel is a simple panel used as a container for two components :<br/>
- * The application combobox to select available applications. <br/>
- * The AuthTab panels displaying application's meta-groups.<br/>
- * @author Gary van Woerkens
- * @version 1.0
- */
-Ext.ux.Html5Connector = function(config) {
-
-};
-
-Ext.extend(Ext.ux.Html5Connector, Ext.util.Observable, {
-
-  initComponent:function() {
-
-    Ext.ux.Html5Connector.superclass.initComponent.call(this);
-
   }
 
 });
